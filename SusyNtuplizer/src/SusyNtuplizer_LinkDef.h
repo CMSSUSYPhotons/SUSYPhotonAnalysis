8cc5cd63690ae423925837bc1b1fb5715803a5e4
yiiyama <>
2013-04-11 16:33:55 +0000

    cleanup and adaptation to new flattened susyTree format

--- SusyNtuplizer/src/SusyNtuplizer_LinkDef.h
+++ SusyNtuplizer/src/SusyNtuplizer_LinkDef.h
@@ -10,81 +10,81 @@
 */
 //
 // Original Author:  Dongwook Jang
-// $Id: SusyNtuplizer_LinkDef.h,v 1.8 2011/10/31 19:57:42 dwjang Exp $
+// $Id: SusyNtuplizer_LinkDef.h,v 1.9 2012/11/28 12:01:04 yiiyama Exp $
 //
 
 #include "../src/SusyEvent.h"
 
 #ifdef __CINT__
 
-#pragma link off all   globals;
-#pragma link off all   classes;
-#pragma link off all   functions;
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link off all typedefs;
 
-#pragma link C++ class  std::pair<Int_t, UChar_t>+;
-#pragma link C++ class  std::map<TString, UChar_t>+;
-#pragma link C++ class  std::map<TString, Float_t>+;
-#pragma link C++ class  std::map<TString, std::pair<Int_t, UChar_t> >+;
-#pragma link C++ class  std::pair<TString, std::pair<Int_t, UChar_t> >+;
-#pragma link C++ class  std::map<TString, susy::MET>+;
-#pragma link C++ class  std::map<TString, TVector3>+;
-#pragma link C++ class  std::map<TString, TLorentzVector>+;
-#pragma link C++ class  std::pair<TString, UChar_t>+;
-#pragma link C++ class  std::pair<TString, Float_t>+;
-#pragma link C++ class  std::pair<TString, susy::MET>+;
-#pragma link C++ class  std::pair<TString, TVector3>+;
-#pragma link C++ class  std::pair<TString, TLorentzVector>+;
-#pragma link C++ class  std::pair<unsigned int,bool>+;
-#pragma link C++ class  std::pair<TString,int>+;
-#pragma link C++ class  std::pair<TString,bool>+;
-#pragma link C++ class  std::pair<TString,double>+;
-#pragma link C++ class  std::pair<unsigned int,double>+;
-#pragma link C++ class  std::pair<unsigned int,int>+;
-#pragma link C++ class  susy::PUSummaryInfo+;
-#pragma link C++ class  susy::Particle+;
-#pragma link C++ class  susy::CorrMETData+;
-#pragma link C++ class  susy::MET+;
-#pragma link C++ class  susy::Vertex+;
-#pragma link C++ class  susy::Cluster+;
-#pragma link C++ class  susy::SuperCluster+;
-#pragma link C++ class  susy::Track+;
-#pragma link C++ class  susy::Muon+;
-#pragma link C++ class  susy::Photon+;
-#pragma link C++ class  susy::Electron+;
-#pragma link C++ class  susy::CaloJet+;
-#pragma link C++ class  susy::PFJet+;
-#pragma link C++ class  susy::JPTJet+;
-#pragma link C++ class  susy::PFParticle+;
-#pragma link C++ class  std::vector<susy::Vertex>+;
-#pragma link C++ class  std::vector<susy::Track>+;
-#pragma link C++ class  std::vector<susy::SuperCluster>+;
-#pragma link C++ class  std::vector<susy::Cluster>+;
-#pragma link C++ class  std::vector<susy::Muon>+;
-#pragma link C++ class  std::vector<susy::Electron>+;
-#pragma link C++ class  std::vector<susy::Photon>+;
-#pragma link C++ class  std::vector<susy::CaloJet>+;
-#pragma link C++ class  std::vector<susy::PFJet>+;
-#pragma link C++ class  std::vector<susy::JPTJet>+;
-#pragma link C++ class  std::vector<susy::PFParticle>+;
-#pragma link C++ class  std::vector<susy::Particle>+;
-#pragma link C++ class  std::vector<susy::PUSummaryInfo>+;
-#pragma link C++ class  std::vector<TVector3>+;
-#pragma link C++ class  std::map<TString, std::vector<susy::Electron> >+;
-#pragma link C++ class  std::map<TString, std::vector<susy::Photon> >+;
-#pragma link C++ class  std::map<TString, std::vector<susy::CaloJet> >+;
-#pragma link C++ class  std::map<TString, std::vector<susy::PFJet> >+;
-#pragma link C++ class  std::map<TString, std::vector<susy::JPTJet> >+;
-#pragma link C++ class  std::map<TString, std::vector<susy::PFParticle> >+;
-#pragma link C++ class  susy::Event+;
-#pragma link C++ class  std::map<TString, std::map<unsigned int, int> >+;
-#pragma link C++ class  std::map<unsigned int, int>+;
-#pragma link C++ class  std::map<TString, std::map<unsigned int, bool> >+;
-#pragma link C++ class  std::map<unsigned int, bool>+;
-#pragma link C++ class  std::map<TString, int>+;
-#pragma link C++ class  std::map<TString, bool>+;
-#pragma link C++ class  std::map<TString, double>+;
-#pragma link C++ class  std::map<TString, std::map<unsigned int, double> >+;
-#pragma link C++ class  std::map<unsigned int, double>+;
-#pragma link C++ class  std::vector<int>+;
+//// LINKS NEEDED TO BE ABLE TO WRITE SUSYTREE
+
+#pragma link C++ class std::pair<TString, UChar_t>+;
+#pragma link C++ class std::map<TString, UChar_t>+;
+#pragma link C++ class std::pair<TString, Float_t>+;
+#pragma link C++ class std::map<TString, Float_t>+;
+#pragma link C++ class std::pair<TString, TVector3>+;
+#pragma link C++ class std::map<TString, TVector3>+;
+#pragma link C++ class std::pair<TString, TLorentzVector>+;
+#pragma link C++ class std::map<TString, TLorentzVector>+;
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+#pragma link C++ namespace susy;
+
+#pragma link C++ class susy::PUSummaryInfo+;
+#pragma link C++ class susy::PUSummaryInfoCollection+;
+#pragma link C++ class susy::Particle+;
+#pragma link C++ class susy::ParticleCollection+;
+#pragma link C++ class susy::MET+;
+#pragma link C++ class susy::Vertex+;
+#pragma link C++ class susy::VertexCollection+;
+#pragma link C++ class susy::Cluster+;
+#pragma link C++ class susy::ClusterCollection+;
+#pragma link C++ class susy::SuperCluster+;
+#pragma link C++ class susy::SuperClusterCollection+;
+#pragma link C++ class susy::Track+;
+#pragma link C++ class susy::TrackCollection+;
+#pragma link C++ class susy::Muon+;
+#pragma link C++ class susy::MuonCollection+;
+#pragma link C++ class susy::Electron+;
+#pragma link C++ class susy::ElectronCollection+;
+#pragma link C++ class susy::Photon+;
+#pragma link C++ class susy::PhotonCollection+;
+#pragma link C++ class susy::CaloJet+;
+#pragma link C++ class susy::CaloJetCollection+;
+#pragma link C++ class susy::PFJet+;
+#pragma link C++ class susy::PFJetCollection+;
+#pragma link C++ class susy::JPTJet+;
+#pragma link C++ class susy::JPTJetCollection+;
+#pragma link C++ class susy::PFParticle+;
+#pragma link C++ class susy::PFParticleCollection+;
+
+//// LINKS NEEDED TO BE ABLE TO READ SUSYTREE FROM INTERACTIVE SESSION
+
+#pragma link C++ enum susy::MetFilters;
+#pragma link C++ enum susy::BTagDiscriminators;
+#pragma link C++ enum susy::PUJetIdAlgorithms;
+
+#pragma link C++ class std::vector<const susy::Cluster*>+;
+#pragma link C++ class std::vector<const susy::Track*>+;
+#pragma link C++ class std::vector<const susy::PFParticle*>+;
+
+#pragma link C++ class std::map<TString, susy::MET>+;
+#pragma link C++ class std::map<TString, susy::MuonCollection>+;
+#pragma link C++ class std::map<TString, susy::ElectronCollection>+;
+#pragma link C++ class std::map<TString, susy::PhotonCollection>+;
+#pragma link C++ class std::map<TString, susy::CaloJetCollection>+;
+#pragma link C++ class std::map<TString, susy::PFJetCollection>+;
+#pragma link C++ class std::map<TString, susy::JPTJetCollection>+;
+#pragma link C++ class std::map<TString, susy::PFParticleCollection>+;
+#pragma link C++ class std::pair<UInt_t, Bool_t>+;
+#pragma link C++ class susy::TriggerMap+;
+#pragma link C++ class susy::Event+;
 
 #endif
