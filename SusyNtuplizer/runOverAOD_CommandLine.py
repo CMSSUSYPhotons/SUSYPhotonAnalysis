056788e2e4ecbad0d847d76fd617f1f2d0b44958
Knut <maximilian.kiesel@rwth-aachen.de>
2013-07-26 12:21:41 +0200

    updated help message

--- SusyNtuplizer/runOverAOD_CommandLine.py
+++ SusyNtuplizer/runOverAOD_CommandLine.py
@@ -1,6 +1,6 @@
 from runOverAOD import configure
 import FWCore.ParameterSet.VarParsing as VarParsing
-# Call by e.g. "cmsRun runOverAOD_SinglePhoton.py dataset=53x22Jan2013 inputFiles=file1.root file2.root"
+# Call by e.g. "cmsRun runOverAOD_CommandLine.py dataset=53x22Jan2013 inputFiles=file1.root file2.root"
 
 
 ## get command line arguments
